356bb802ba5a1a67c0a6933987c4576f721c3a8d
Mohammad-Ali <mohammad@test-share.local>
2019-01-13 00:45:44 -0500

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var dispatcher=require("/MEME/Dispatcher/index.js");
 dispatcher.setUP(80,'172.104.29.98');
-dispatcher.staticDirectory('/static/','/MEME/MEME/static');
+dispatcher.staticDirectory('/static','/MEME/MEME/static');
 dispatcher.GetRequest('/',function(req,res){
             res.writeHead(302, {
                 'Cache-Control':'no-cache, no-store, must-revalidate',
