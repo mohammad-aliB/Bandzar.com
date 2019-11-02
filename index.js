e4c31d820077263776ef0f2ff338989e0635bfd6
Mohammad-Ali <mohammad@test-share.local>
2019-11-01 23:17:45 -0400

    added favicon

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ try{
 var dispatcher=require("/MEME/Dispatcher/index.js");//should be changed in later revision
 dispatcher.setUP(80,'172.104.29.98');
 dispatcher.staticDirectory('/static','/bandzar.com/bandzar.com/static');
+dispatcher.GetRequest('/favicon.ico',function(req,res){
+            res.writeHead(302, {
+                'Cache-Control':'no-cache, no-store, must-revalidate',
+                'Pragma':'no-cache',
+                'Expires':'0',
+                'Location': '/static/favicon.ico',
+            });
+            res.end();
+        });
 dispatcher.GetRequest('/',function(req,res){
             res.writeHead(302, {
                 'Cache-Control':'no-cache, no-store, must-revalidate',
