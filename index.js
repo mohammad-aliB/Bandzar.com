9abd93a1c7869c8d3b9b1d4f3924194b34b15487
Mohammad-Ali <mohammad@test-share.local>
2020-10-08 17:14:24 -0400

    added hello world

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ dispatcher.GetRequest('/',function(req,res){
             });
             res.end();
         });
+dispatcher.GetRequest('/STA256-A1',function(req,res){
+            res.writeHead(302, {
+                'Cache-Control':'no-cache, no-store, must-revalidate',
+                'Pragma':'no-cache',
+                'Expires':'0',
+            });
+            res.write("hello world");
+            res.end();
+        });
 console.log("the server started successfully");
 }catch(err) {
 console.log("there was this error: "+err);
